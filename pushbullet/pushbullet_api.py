4a26a8ffc6b6dd14edd0e5b0be8687345edc05ce
sean8900 <s3806690@student.rmit.edu.au>
2020-06-12 15:08:21 +1000

    updated pushbullet script

--- pushbullet/pushbullet_api.py
+++ pushbullet/pushbullet_api.py
@@ -2,10 +2,13 @@ from pushbullet import Pushbullet
 
 class PushBulletAPI:
     
+    def main(self):
+        self.pushNotification(10, "Toyota")
+
     def __init__(self):
         self.pb = Pushbullet('o.BfDh4Z35MEnCyxal5J0dcIuMamGyU38a')
 
     def pushNotification(self, carID, car_make):
         push = self.pb.push_note("Car require repair", "Car ID: "+carID+" ,Car Make: "+car_make)
         
-
+PushBulletAPI().main()
